d884f3614569be99e053f7d4af48b3739696ff3f
qoranreh <qoranreh@gmail.com>
2024-11-19 01:29:18 +0900

    add CalendarPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 
 void main() {
-  runApp(const MyApp());
+  initializeDateFormatting().then((_) => runApp(MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -12,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       home: const TimetablePage(),
     );
   }
@@ -25,15 +28,16 @@ class TimetablePage extends StatefulWidget {
   @override
   State<TimetablePage> createState() => _TimetablePageState();
 }
+
 //타임테이블 페이지 스테이트 관리 - 엥 여기가 다 감싸고 있는데?
 class _TimetablePageState extends State<TimetablePage> {
   //state저장
   final int hoursInDay = 24;
   final List<String> days = ['월', '화', '수', '목', '금', '토', '일'];
   late final PageController pageController;
-  List<List<List<String>>> timetable = List.generate(7, (index) => List.generate(24, (i) => []));
+  List<List<List<String>>> timetable =
+      List.generate(7, (index) => List.generate(24, (i) => []));
   String currentDay = '월';
-
 
   @override
   //initstate? 있었는데 didChangeDepen~ 으로 변경함.
@@ -68,7 +72,8 @@ class _TimetablePageState extends State<TimetablePage> {
     });
   }
 
-  void editTask(BuildContext context, int dayIndex, int hourIndex, int taskIndex) {
+  void editTask(
+      BuildContext context, int dayIndex, int hourIndex, int taskIndex) {
     TextEditingController textController = TextEditingController();
     showDialog(
       context: context,
@@ -83,7 +88,8 @@ class _TimetablePageState extends State<TimetablePage> {
             TextButton(
               onPressed: () {
                 setState(() {
-                  timetable[dayIndex][hourIndex][taskIndex] = textController.text;
+                  timetable[dayIndex][hourIndex][taskIndex] =
+                      textController.text;
                 });
                 Navigator.of(context).pop();
               },
@@ -116,7 +122,6 @@ class _TimetablePageState extends State<TimetablePage> {
     return summary;
   }
 
-
   void showSummaryModal(BuildContext context) {
     Map<String, int> summary = getTaskSummary();
     showModalBottomSheet(
@@ -136,66 +141,18 @@ class _TimetablePageState extends State<TimetablePage> {
     );
   }
 
-  void _showCustomModal(BuildContext context) {
-    showGeneralDialog(
-      context: context,
-      barrierDismissible: true,
-      barrierLabel: MaterialLocalizations.of(context).modalBarrierDismissLabel,
-      barrierColor: Colors.black54, // 배경 색상
-      transitionDuration: const Duration(milliseconds: 300),
-      pageBuilder: (context, animation, secondaryAnimation) {
-        return Align(
-          alignment: Alignment.topCenter,
-          child: Container(
-            height: 700, // 전체 모달창 크기
-            width: MediaQuery.of(context).size.width , // 좌측 여백 설정
-            margin: const EdgeInsets.only(top: 0),
-            padding: const EdgeInsets.only(top:50), // 내부 여백 설정
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(43),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                // 제목 및 텍스트 영역
-                Column(
-                  children: [
-                    Text(
-                      "Custom Modal",
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.red,
-                      ),
-                    ),
-                    SizedBox(height: 20), // 제목과 문장 사이의 간격
-                    Text(
-                      "This modal slides from the top!",
-                      style: TextStyle(fontSize: 16), // 적절한 폰트 크기 설정
-                      textAlign: TextAlign.center, // 중앙 정렬
-                    ),
-                  ],
-                ),
-                // 버튼 영역
-                ElevatedButton(
-                  onPressed: () => Navigator.of(context).pop(),
-                  style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.symmetric(horizontal: 32, vertical: 12),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                  ),
-                  child: Text("Close"),
-                ),
-              ],
-            ),
-          ),
-        );
-      },
-      transitionBuilder: (context, animation, secondaryAnimation, child) {
-        final offsetAnimation =
-        Tween<Offset>(begin: Offset(0, -1), end: Offset(0, 0)).animate(animation);
+  PageRouteBuilder _createCalendarPageRoute() {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => CalendarPage(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(0.0, -1.0); // 위에서 아래로
+        const end = Offset.zero;
+        const curve = Curves.easeInOut;
+
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+        var offsetAnimation = animation.drive(tween);
+
         return SlideTransition(
           position: offsetAnimation,
           child: child,
@@ -216,9 +173,10 @@ class _TimetablePageState extends State<TimetablePage> {
             onPressed: () => showSummaryModal(context),
           ),
           IconButton(
-            icon: const Icon(Icons.calendar_month),
-              onPressed: () => _showCustomModal(context)),
-
+              icon: const Icon(Icons.calendar_month),
+              onPressed: () {
+                Navigator.of(context).push(_createCalendarPageRoute());
+              }),
         ],
       ),
       body: PageView.builder(
@@ -244,11 +202,14 @@ class _TimetablePageState extends State<TimetablePage> {
                         scrollDirection: Axis.horizontal,
                         itemCount: timetable[dayIndex][hourIndex].length,
                         itemBuilder: (context, taskIndex) {
-                          String task = timetable[dayIndex][hourIndex][taskIndex];
+                          String task =
+                              timetable[dayIndex][hourIndex][taskIndex];
                           return Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 4.0),
                             child: TextButton(
-                              onPressed: () => editTask(context, dayIndex, hourIndex, taskIndex),
+                              onPressed: () => editTask(
+                                  context, dayIndex, hourIndex, taskIndex),
                               child: Text(task),
                             ),
                           );
@@ -262,7 +223,78 @@ class _TimetablePageState extends State<TimetablePage> {
           );
         },
       ),
+    );
+  }
+}
 
+class CalendarPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Calendar Page"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: TableCalendar(
+          calendarBuilders: CalendarBuilders(dowBuilder: (context, day) {
+            switch (day.weekday) {
+              case 1:
+                return const Center(
+                  child: Text('월'),
+                );
+              case 2:
+                return const Center(
+                  child: Text('화'),
+                );
+              case 3:
+                return const Center(
+                  child: Text('수'),
+                );
+              case 4:
+                return const Center(
+                  child: Text('목'),
+                );
+              case 5:
+                return const Center(
+                  child: Text('금'),
+                );
+              case 6:
+                return const Center(
+                  child: Text('토'),
+                );
+              case 7:
+                return const Center(
+                  child: Text(
+                    '일',
+                    style: TextStyle(color: Colors.red),
+                  ),
+                );
+            }
+          }),
+          firstDay: DateTime.utc(2020, 1, 1),
+          lastDay: DateTime.utc(2030, 12, 31),
+          locale: 'ko-KR',
+          focusedDay: DateTime.now(),
+          calendarStyle: const CalendarStyle(
+            todayDecoration: BoxDecoration(
+              color: Colors.blueAccent,
+              shape: BoxShape.circle,
+            ),
+            selectedDecoration: BoxDecoration(
+              color: Colors.redAccent,
+              shape: BoxShape.circle,
+            ),
+          ),
+          headerStyle: HeaderStyle(
+            formatButtonVisible: false, // 포맷 버튼 숨김
+            titleCentered: true,
+          ),
+          onDaySelected: (selectedDay, focusedDay) {
+            print("Selected day: $selectedDay");
+          },
+        ),
+      ),
     );
   }
 }
